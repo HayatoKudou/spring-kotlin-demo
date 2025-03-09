69b50a38395dc0fe5467baa986ff4393f6d87510
工藤颯斗 <kudo@tetemarche.co.jp>
2025-03-09 16:08:38 +0900

    feat: DBと接続、CRUDを完全に理解

---
+++ src/main/kotlin/com/example/demo/MessageService.kt
@@ -0,0 +1,21 @@
+package com.example.demo
+
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+import java.util.*
+
+@Service
+class MessageService(private val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message): Message {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values ( ?, ? )",
+            id, message.text
+        )
+        return message.copy(id = id)
+    }
+}
