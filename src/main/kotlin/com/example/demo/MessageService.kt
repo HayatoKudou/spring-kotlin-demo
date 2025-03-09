e650ecb5c354b7ecd7c375a2510e16a0b4df9377
工藤颯斗 <kudo@tetemarche.co.jp>
2025-03-09 16:25:08 +0900

    feat: DBと接続、CRUDを完全に理解-2

--- src/main/kotlin/com/example/demo/MessageService.kt
+++ src/main/kotlin/com/example/demo/MessageService.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import org.springframework.stereotype.Service
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
 import java.util.*
 
 @Service
@@ -9,6 +10,10 @@ class MessageService(private val db: JdbcTemplate) {
     fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
         Message(response.getString("id"), response.getString("text"))
     }
+
+    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id) { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }.singleOrNull()
 
     fun save(message: Message): Message {
         val id = message.id ?: UUID.randomUUID().toString()
